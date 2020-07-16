bc8e4e4fbee19a73abe530d49eeb9a656f196d2b
Josh Menden <josh.menden@gmail.com>
2020-07-15 18:30:55 -0600

    First commit with stubbed classes and files

---
+++ hangman.rb
@@ -0,0 +1,12 @@
+# while word is not
+# Turn class
+
+# Hangman class
+    # word
+    # strikes
+      # map to body parts?
+    # previously guessed letters
+    # known letters
+
+
+# Some kind of User Input class for easy CLI managment
